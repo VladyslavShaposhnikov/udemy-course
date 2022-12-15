90bb388f1cfe403cc690de4a545fa6f9cc4e4505
VladyslavShaposhnikov <shapik2404@gmail.com>
2022-12-15 22:55:57 +0100

    create all post and post detail html files, html structure in index.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, HttpResponse
 from django.http import request
 
+post_list = {
+    'post 1': 'Thats my first post',
+    'post 2': 'Thats my second post',
+    'post 3': 'Thats my thirth post',
+    'post 4': 'Thats my forth post',
+}
+
 def index(request):
     return HttpResponse(render(request, 'blog/index.html'))
+
+def posts(request):
+    titles = list(post_list.keys())
+    return HttpResponse(render(request, 'blog/all_posts.html', {'titles': titles, 'post_list': post_list}))
+
+def post_detail(request, slug):
+    #post = slug
+    return HttpResponse(render(request, 'blog/post_detail.html'))
