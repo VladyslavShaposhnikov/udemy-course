98bb4604f7d4715ff1880de33c12ec2311e11a76
VladyslavShaposhnikov <shapik2404@gmail.com>
2023-02-18 17:47:47 +0100

    Tag model changes

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=90)
+
+    def __str__(self):
+        return f"{self.caption}"
+
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -18,13 +24,8 @@ class Posts(models.Model):
     slug = models.SlugField(unique=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts')
+    tags = models.ManyToManyField(Tag)
     
     def __str__(self):
         return f"{self.title}, {self.date}"
     
-class Tag(models.Model):
-    post = models.ManyToManyField(Posts)
-    caption = models.CharField(max_length=90)
-
-    def __str__(self):
-        return f"{self.caption}"
