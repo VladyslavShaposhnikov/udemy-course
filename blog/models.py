4c17f4a9dfa67beadf884e5160b1502278012ba6
VladyslavShaposhnikov <shapik2404@gmail.com>
2023-02-18 17:45:43 +0100

    Author model changes

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Posts(models.Model):
     title = models.CharField(max_length=90)
     excerpt = models.CharField(max_length=150)
@@ -9,18 +17,10 @@ class Posts(models.Model):
     date = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts')
     
     def __str__(self):
         return f"{self.title}, {self.date}"
-    
-class Author(models.Model):
-    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
     
 class Tag(models.Model):
     post = models.ManyToManyField(Posts)
