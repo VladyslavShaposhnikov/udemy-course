21ed7fca6c0536946e67bfc6ac4c57e0617fd4c8
VladyslavShaposhnikov <shapik2404@gmail.com>
2023-03-29 02:20:00 +0200

    handling comment form submission

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 from .forms import CommentForm
@@ -21,15 +25,36 @@ class PostsListView(ListView):
     ordering = ["-date"]
     context_object_name = "all_posts"
 
-class PostDetailView(DetailView):
+class PostDetailView(View):
     template_name = "blog/post_detail.html"
     model = Post
+
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        context = {
+            "post": post,
+            "tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post_detail.html", context)
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = get_object_or_404(Post, slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail", args=[slug]))
+        
+        post = get_object_or_404(Post, slug=slug)
+        context = {
+            "post": post,
+            "tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post_detail.html", context)
+
 
 class TagPostsView(ListView):
     template_name = "blog/tag_posts.html"
