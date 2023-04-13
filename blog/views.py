61821e9f101922f140f1069e63bf81e7de1885f7
VladyslavShaposhnikov <shapik2404@gmail.com>
2023-04-13 02:12:29 +0200

    removing from reed later

--- blog/views.py
+++ blog/views.py
@@ -26,16 +26,24 @@ class PostsListView(ListView):
     context_object_name = "all_posts"
 
 class PostDetailView(View):
-    template_name = "blog/post_detail.html"
-    model = Post
+    def is_stored_posts(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+
+        return is_saved_for_later
 
     def get(self, request, slug):
         post = get_object_or_404(Post, slug=slug)
+        
         context = {
             "post": post,
             "tags": post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_posts(request, post.id)
         }
         return render(request, "blog/post_detail.html", context)
     
@@ -53,7 +61,8 @@ class PostDetailView(View):
             "post": post,
             "tags": post.tags.all(),
             "comment_form": comment_form,
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_posts(request, post.id)
         }
         return render(request, "blog/post_detail.html", context)
 
@@ -96,6 +105,9 @@ class ReadLaterView(View):
 
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+            
+        request.session["stored_posts"] = stored_posts
 
         return HttpResponseRedirect("/")
